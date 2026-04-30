df6b76b524c8bb591918e335a10a482f608e8684
tii-mom <103726191+tii-mom@users.noreply.github.com>
2026-04-30 16:23:23 +0800

    fix(frontend): bootstrap app after browser polyfills

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,3 @@
 import './lib/browserPolyfills';
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+void import('./bootstrap');
